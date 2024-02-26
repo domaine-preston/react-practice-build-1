2278fc04cf70377fec6a037760ca0c5cd37a0f12
Preston Roberts <preston@tomorrowagency.com>
2024-02-26 15:43:59 -0700

    adding files from base theme

---
+++ tailwind-token-library.js
@@ -0,0 +1,44 @@
+// Assumes base font 16rem for REM
+
+module.exports = {
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1024px',
+    },
+    fontSize: {
+      0: '0',
+      8: '0.5rem',
+      10: '0.625rem',
+      12: '0.75rem',
+      14: '0.875rem',
+      16: '1rem',
+      18: '1.125rem',
+      20: '1.25rem',
+      22: '1.375rem',
+      24: '1.5rem',
+      28: '1.75rem',
+      36: '2.25rem',
+      44: '2.75rem',
+      48: '3rem',
+    },
+    lineHeight: {
+      100: '100%',
+      110: '110%',
+      120: '120%',
+      130: '130%',
+      140: '140%',
+      150: '150%',
+      160: '160%',
+    },
+    letterSpacing: {
+      xs: '0',
+      'xs-s': '0.5px',
+      s: '1px',
+      m: '2px',
+      l: '3px',
+      xl: '4px',
+    },
+  },
+};
