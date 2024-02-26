438c8d8850d257e1f58a1c131bbd50790bf89ca4
Preston Roberts <preston@tomorrowagency.com>
2024-02-26 15:48:11 -0700

    updating eslint and prettier line lengths from 80 to 120

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     'max-len': [
       'error',
       {
-        code: 80,
+        code: 120,
         tabWidth: 2,
         ignoreComments: true,
         ignoreStrings: true,
